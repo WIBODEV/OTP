b4b80c038f173c59fb764d7f9d00005fe96beb44
WIBODEV <79251993+WIBODEV@users.noreply.github.com>
2021-02-18 10:28:18 +0100

    Update credential.php

--- credential.php
+++ credential.php
@@ -1,6 +1,6 @@
 <?php 
 	/*Enter your API_KEY*/
-	define("API_KEY", '');
+	define("API_KEY", '6uP3+O/Po/I-aadfTFRJRoqvvbThkQi98VKWXWr6CX');
 
 	/*You can enter mobile number here*/
 	define("MOBILE", '');
